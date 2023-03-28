346b3f509fc4c0e4208934c041531b1d6ca408f5
akash-singha-3558 <akashsingha@gmail.com>
2023-03-29 00:16:06 +0530

    app.js file prettified

--- server/app.js
+++ server/app.js
@@ -5,13 +5,12 @@ const adminRouter=require("./controllers/auth.routes.js");
 const adminUserRoutes=require("./controllers/admin.users.tables.routes.js");
 const adminProductRouter=require("./controllers/admin.products.routes.js");
 require("dotenv").config();
+
 app.use(express.json())
-app.get("/",(req,res)=>{
-    res.status(200).send({message:"home"})
-})
+
 
 app.use("/auth",adminRouter);
 app.use("/adminproduct",adminProductRouter);
 app.use("/adminuser",adminUserRoutes);
 
-app.listen(process.env.port,connect)
+app.listen(process.env.port,connect);
