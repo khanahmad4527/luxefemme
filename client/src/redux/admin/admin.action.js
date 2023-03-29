4cf6a41ac326bd431c39d8dc9228b4603dae8d34
akash-singha-3558 <akashsingha@gmail.com>
2023-03-29 22:40:04 +0530

    redux part of admin site is fully completed

---
+++ client/src/redux/admin/admin.action.js
@@ -0,0 +1,187 @@
+import {
+    ADD_PRODUCT_LOADING,
+    ADD_PRODUCT_SUCCESS,
+    ADD_PRODUCT_ERROR,
+    DELETE_PRODUCT_ERROR,
+    DELETE_PRODUCT_LOADING,
+    DELETE_PRODUCT_SUCCESS,
+    GET_PRODUCTS_ERROR,
+    GET_PRODUCTS_LOADING,
+    GET_PRODUCTS_SUCCESS,
+    GET_USERS_LOADING,
+    GET_USERS_ERROR,
+    GET_USERS_SUCCESS,
+    DELETE_USERS_LOADING,
+    DELETE_USERS_ERROR,
+    DELETE_USERS_SUCCESS,
+    ADD_ADMIN_LOADING,
+    ADD_ADMIN_ERROR,
+    ADD_ADMIN_SUCCESS,
+    DELETE_ADMIN_LOADING,
+    DELETE_ADMIN_ERROR,
+    DELETE_ADMIN_SUCCESS,
+    PATCH_PRODUCT_LOADING,
+    PATCH_PRODUCT_ERROR,
+    PATCH_PRODUCT_SUCCESS,
+    GET_ADMIN_LOADING,
+    GET_ADMIN_ERROR,
+    GET_ADMIN_SUCCESS
+} from "./admin.types";
+
+import { getProduct, delProduct, delAdmin, delUser, getAdmin, getUsers, addAdmin, updateProduct, postProduct } from "./admin.api"
+
+
+//Products===============================================================================
+
+//add
+export const addProductsLoading = () => ({
+    type: ADD_PRODUCT_LOADING
+})
+
+
+export const addProductsError = () => ({
+    type: ADD_PRODUCT_ERROR
+})
+
+
+export const addProductsSucess = (payload) => (dispatch) => {
+    dispatch(addProductsLoading())
+    postProduct(payload).then((res) => { dispatch({ type: ADD_PRODUCT_SUCCESS, payload: payload }) }).catch((er) => {
+
+        dispatch(addProductsError())
+
+    })
+
+}
+
+
+
+//delete
+export const deleteProductLoading = () => ({
+    type: DELETE_PRODUCT_LOADING
+})
+
+export const deleteProductError = () => ({
+    type: DELETE_PRODUCT_ERROR
+})
+
+export const deleteProductSucess = (id) => (dispatch) => {
+    dispatch(deleteProductLoading());
+    delProduct(id).then((res) => {
+        dispatch({ type: DELETE_PRODUCT_SUCCESS })
+    }).catch((er) => {
+        dispatch({ type: DELETE_PRODUCT_ERROR })
+    })
+}
+
+
+
+//get product
+export const getProductLoading = () => ({
+    type: GET_PRODUCTS_LOADING
+})
+
+export const getProductError = () => ({
+    type: GET_PRODUCTS_ERROR
+})
+
+export const getProductSuccess = () => (dispatch) => {
+    dispatch({ type: GET_PRODUCTS_ERROR })
+    getProduct().then((res) => {
+        dispatch({ type: GET_PRODUCTS_SUCCESS, payload: res })
+    }).catch((er) => {
+        dispatch({ type: GET_PRODUCTS_ERROR })
+    })
+}
+
+
+
+//update product
+
+export const updateProductLoading = () => ({ type: PATCH_PRODUCT_LOADING });
+export const updateProductError = () => ({ tye: PATCH_PRODUCT_ERROR });
+
+export const updateProductSuccess = (payload) => (dispatch) => {
+    dispatch(updateProductLoading())
+    updateProduct(payload.id, payload).then((res) => {
+        dispatch({ type: PATCH_PRODUCT_SUCCESS });
+    }).catch((er) => {
+        dispatch(updateProductError())
+    });
+
+
+
+}
+
+
+
+
+
+//users==========================================================
+
+export const getUserLoading = () => ({
+    type: GET_USERS_LOADING
+})
+
+export const getUsersError = () => ({
+    type: GET_USERS_ERROR
+})
+
+export const getUserSuccess = () => (dispatch) => {
+    dispatch(getUserLoading());
+    getUsers.then((res) => {
+        dispatch({ type: GET_USERS_SUCCESS, payload: res })
+    }).catch((er) => {
+        dispatch({ type: GET_USERS_ERROR })
+    })
+
+}
+
+export const delUserLoading = () => ({ type: DELETE_USERS_LOADING });
+export const delUserEror = () => ({ type: DELETE_USERS_ERROR });
+export const delUserSuccess = (id) => (dispatch) => {
+    dispatch(delUserLoading());
+    delUser(id).then((res) => {
+        dispatch({ type: DELETE_USERS_SUCCESS });
+    }).catch((er) => {
+        dispatch(delUserEror())
+    })
+}
+
+
+//admin================================================================
+
+export const getAdminLoading = () => ({ type: GET_ADMIN_LOADING })
+export const getAdminError = () => ({ type: GET_ADMIN_ERROR })
+
+export const getAdminSuccess = () => (dispatch) => {
+    dispatch(getAdminLoading());
+    getAdmin().then((res) => {
+        dispatch({ type: GET_ADMIN_SUCCESS, payload: res })
+    }).catch((er) => { dispatch(getAdminError()) })
+}
+
+
+
+export const addAdminLoading = () => ({ type: ADD_ADMIN_LOADING });
+export const addAdminError = () => ({ type: ADD_ADMIN_ERROR });
+
+export const addAdminSuccess = (payload) => (dispatch) => {
+    dispatch(addAdminLoading());
+    addAdmin(payload).then((res) => {
+        dispatch({ type: ADD_ADMIN_SUCCESS, payload })
+    }).catch((er) => {
+        dispatch(addAdminError())
+    })
+}
+
+export const delAdminLoading = () => ({ type: DELETE_ADMIN_LOADING });
+export const delAdminError = () => ({ type: DELETE_ADMIN_ERROR });
+
+export const delAdminSuccess = (id) => (dispatch) => {
+    dispatch(delAdminLoading())
+    delAdmin(id).then((res) => {
+        dispatch({ type: DELETE_ADMIN_SUCCESS });
+    }).catch((er) => { dispatch(delAdminError()) })
+
+}
--- client/src/redux/admin/admin.types.js
+++ client/src/redux/admin/admin.types.js
@@ -13,3 +13,27 @@ export const PATCH_PRODUCT_ERROR = "patch/product/error";
 export const DELETE_PRODUCT_LOADING = "delete/product/loading";
 export const DELETE_PRODUCT_SUCCESS = "delete/product/success";
 export const DELETE_PRODUCT_ERROR = "delete/product/error";
+
+export const GET_USERS_LOADING="get/users/loading";
+export const GET_USERS_ERROR="get/users/error";
+export const GET_USERS_SUCCESS="get/users/success";
+
+export const DELETE_USERS_LOADING="delete/users/loading";
+export const DELETE_USERS_ERROR="delete/users/error";
+export const DELETE_USERS_SUCCESS="delete/users/success";
+
+export const ADD_ADMIN_LOADING="add/admin/loading";
+export const ADD_ADMIN_ERROR="add/admin/error";
+export const ADD_ADMIN_SUCCESS="add/admin/success";
+
+export const GET_ADMIN_LOADING="get/admin/loading";
+export const GET_ADMIN_ERROR="get/admin/error";
+export const GET_ADMIN_SUCCESS="get/admin/success";
+
+
+export const DELETE_ADMIN_LOADING="delete/admin/loading";
+export const DELETE_ADMIN_ERROR="delete/admin/error";
+export const DELETE_ADMIN_SUCCESS="delete/admin/success";
+
+
+
