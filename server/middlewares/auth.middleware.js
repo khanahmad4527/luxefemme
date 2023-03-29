afbb567316dca35ccb83cfd0770f1b50c19017e8
akash-singha-3558 <akashsingha@gmail.com>
2023-03-29 11:30:29 +0530

    preetified auth middleware

--- server/middlewares/auth.middleware.js
+++ server/middlewares/auth.middleware.js
@@ -1,21 +1,19 @@
-const jwt=require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 
-const auth=(req,res,next)=>{
-let token=req.headers.token;
-if(token){
-jwt.verify(token, process.env.privatekey, (err, decoded)=>{
-   //  console.log(token,decoded.role)
-     if(decoded.role=="admin" || decoded.role=="superadmin"){
-        next()
-     }
-     else{
-        res.status(400).send({error:err})
-     }
-  });
-}
-else{
-    res.status(400).send({error:"You are not authorized"})
-}
-}
+const auth = (req, res, next) => {
+  let token = req.headers.token;
+  if (token) {
+    jwt.verify(token, process.env.privatekey, (err, decoded) => {
+      //  console.log(token,decoded.role)
+      if (decoded.role == "admin" || decoded.role == "superadmin") {
+        next();
+      } else {
+        res.status(400).send({ error: err });
+      }
+    });
+  } else {
+    res.status(400).send({ error: "You are not authorized" });
+  }
+};
 
-module.exports=auth;
+module.exports = auth;
