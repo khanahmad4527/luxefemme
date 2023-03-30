d3288064bba607e3d7c3f668ad628e6e0ad92c11
akash-singha-3558 <akashsingha@gmail.com>
2023-03-30 11:54:18 +0530

    model updated

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,23 +1,16 @@
 const mongoose = require("mongoose");
 const adminUserSchema = mongoose.Schema(
   {
-    username: {
+ 
       firstname: { type: String, required: true },
       lastname: { type: String, required: true },
-    },
-    email: { type: String, required: true },
-    password: { type: String, required: true },
-    age: Number,
+      email: { type: String, required: true },
+      password: { type: String, required: true },
+    
     role: {
       type: String,
       enum: ["admin", "user", "superadmin"],
-    },
-    address: {
-      city: { type: String, required: true },
-      state: { type: String, required: true },
-      country: { type: String, required: true },
-      pin: { type: Number, required: true },
-    },
+    }
   },
   {
     versionKey: false,
