b2bca366ef14198087be362650825bc976ae8e9c
Ahmad Khan <112844940+khanahmad4527@users.noreply.github.com>
2023-04-03 18:39:57 +0530

    Update products.reducer.js

--- client/src/redux/products/products.reducer.js
+++ client/src/redux/products/products.reducer.js
@@ -1,25 +1,41 @@
 import {
   GET_PRODUCTS_LOADING,
   GET_PRODUCTS_SUCCESS,
-  GET_PRODUCTS_ERROR
-} from './products.types';
+  GET_PRODUCTS_ERROR,
+} from "./products.types";
 
 const initialState = {
-  loading: false,
-  error: false,
-  products: []
+  productsIsLoading: false,
+  productsIsError: false,
+  productsData: [],
 };
 
 export const productsReducer = (state = initialState, { type, payload }) => {
   switch (type) {
-    case GET_PRODUCTS_LOADING:
-      return { ...state, loading: true };
+    case GET_PRODUCTS_LOADING: {
+      return {
+        ...state,
+        productsIsLoading: true,
+        productsIsError: false,
+      };
+    }
 
-    case GET_PRODUCTS_SUCCESS:
-      return { ...state, loading: false, products: payload };
+    case GET_PRODUCTS_SUCCESS: {
+      return {
+        ...state,
+        productsIsLoading: false,
+        productsIsError: false,
+        productsData: payload,
+      };
+    }
 
-    case GET_PRODUCTS_ERROR:
-      return { ...state, loading: false, error: true };
+    case GET_PRODUCTS_ERROR: {
+      return {
+        ...state,
+        productsIsLoading: false,
+        productsIsError: true,
+      };
+    }
 
     default: {
       return state;
