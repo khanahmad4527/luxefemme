9827e9a28fb0e1ceca219f472c40937bce223ba0
Ahmad Khan <112844940+khanahmad4527@users.noreply.github.com>
2023-04-03 18:39:29 +0530

    Update products.action.js

--- client/src/redux/products/products.action.js
+++ client/src/redux/products/products.action.js
@@ -1,18 +1,16 @@
+import instance from "../../utils/axiosInstance";
 import {
   GET_PRODUCTS_LOADING,
   GET_PRODUCTS_SUCCESS,
-  GET_PRODUCTS_ERROR
-} from './products.types';
-import instance from '../../utils/axiosInstance';
+  GET_PRODUCTS_ERROR,
+} from "./products.types";
 
-const getProducts = () => async (dispatch) => {
+export const getProducts = () => async (dispatch) => {
   dispatch({ type: GET_PRODUCTS_LOADING });
   try {
-    let res = await instance.get('/products');
-    dispatch({ type: GET_PRODUCTS_SUCCESS, payload: res.data });
+    const responce = await instance.get(`/products`);
+    dispatch({ type: GET_PRODUCTS_SUCCESS, payload: responce.data });
   } catch (err) {
     dispatch({ type: GET_PRODUCTS_ERROR });
   }
 };
-
-export { getProducts };
