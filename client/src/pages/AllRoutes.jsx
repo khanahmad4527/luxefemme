da4b6aa5d603a44335cc0387b244113ab7064c13
khanahmad4527 <khanahmad4527@gmail.com>
2023-10-02 11:16:59 +0530

    added lazyload

--- client/src/pages/AllRoutes.jsx
+++ client/src/pages/AllRoutes.jsx
@@ -1,87 +1,101 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
+import Loading from "../utils/Loading";
 import Login from "../components/Auth/Login";
-import Signup from "../components/Auth/Singup";
-import CartPage from "./CartPage";
-import HomePage from "./HomePage";
-import PageNotFound from "./PageNotFound";
-import ProductDetailPage from "./ProductDetailPage";
-import ProductsPage from "./ProductsPage";
-import CheckoutPage from "./CheckoutPage";
-import AuthProtectedRoutes from "../hoc/AuthProtectedRoutes";
-import OrdersPage from "./OrdersPage";
-import Home from "./AdminPages/Home";
-import Users from "./AdminPages/Users";
-import Admins from "./AdminPages/Admins";
-import Products from "./AdminPages/Products";
-import SimpleSidebar from "../components/Admin/AdminSidebar";
+import Signup from "../components/Auth/Signup";
+import { Square } from "@chakra-ui/react";
+const CartPage = lazy(() => import("./CartPage"));
+const HomePage = lazy(() => import("./HomePage"));
+const PageNotFound = lazy(() => import("./PageNotFound"));
+const ProductDetailPage = lazy(() => import("./ProductDetailPage"));
+const ProductsPage = lazy(() => import("./ProductsPage"));
+const CheckoutPage = lazy(() => import("./CheckoutPage"));
+const AuthProtectedRoutes = lazy(() => import("../hoc/AuthProtectedRoutes"));
+const OrdersPage = lazy(() => import("./OrdersPage"));
+const Home = lazy(() => import("./AdminPages/Home"));
+const Users = lazy(() => import("./AdminPages/Users"));
+const Admins = lazy(() => import("./AdminPages/Admins"));
+const Products = lazy(() => import("./AdminPages/Products"));
+const SimpleSidebar = lazy(() => import("../components/Admin/AdminSidebar"));
 
 const AllRoutes = () => {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/products" element={<ProductsPage />} />
-      <Route path="/product/:id" element={<ProductDetailPage />} />
-      <Route
-        path="/cart"
-        element={
-          <AuthProtectedRoutes>
-            <CartPage />
-          </AuthProtectedRoutes>
-        }
-      />
-      <Route
-        path="/checkout"
-        element={
-          <AuthProtectedRoutes>
-            <CheckoutPage />
-          </AuthProtectedRoutes>
-        }
-      />
-      <Route
-        path="/orders"
-        element={
-          <AuthProtectedRoutes>
-            <OrdersPage />
-          </AuthProtectedRoutes>
-        }
-      />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="*" element={<PageNotFound />} />
-      <Route
-        path="/admin"
-        element={
-          <>
-            <SimpleSidebar /> <Home />
-          </>
-        }
-      />
-      <Route
-        path="/admin/users"
-        element={
-          <>
-            <SimpleSidebar /> <Users />
-          </>
-        }
-      />
-      <Route
-        path="/admin/admins"
-        element={
-          <>
-            <SimpleSidebar /> <Admins />
-          </>
-        }
-      />
-      <Route
-        path="/admin/products"
-        element={
-          <>
-            <SimpleSidebar /> <Products />
-          </>
-        }
-      />
-    </Routes>
+    <Suspense
+      fallback={
+        <Square minH={"100vh"}>
+          <Loading />
+        </Square>
+      }
+    >
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/product/:id" element={<ProductDetailPage />} />
+        <Route
+          path="/cart"
+          element={
+            <AuthProtectedRoutes>
+              <CartPage />
+            </AuthProtectedRoutes>
+          }
+        />
+        <Route
+          path="/checkout"
+          element={
+            <AuthProtectedRoutes>
+              <CheckoutPage />
+            </AuthProtectedRoutes>
+          }
+        />
+        <Route
+          path="/orders"
+          element={
+            <AuthProtectedRoutes>
+              <OrdersPage />
+            </AuthProtectedRoutes>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="*" element={<PageNotFound />} />
+        <Route
+          path="/admin"
+          element={
+            <>
+              <SimpleSidebar />
+              <Home />
+            </>
+          }
+        />
+        <Route
+          path="/admin/users"
+          element={
+            <>
+              <SimpleSidebar />
+              <Users />
+            </>
+          }
+        />
+        <Route
+          path="/admin/admins"
+          element={
+            <>
+              <SimpleSidebar />
+              <Admins />
+            </>
+          }
+        />
+        <Route
+          path="/admin/products"
+          element={
+            <>
+              <SimpleSidebar />
+              <Products />
+            </>
+          }
+        />
+      </Routes>
+    </Suspense>
   );
 };
 
